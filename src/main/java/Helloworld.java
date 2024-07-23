a9c7b252f2da550f74d037a4ede6638b4048222c
Nidhi Lunagariya <112748757+Nidhi16797@users.noreply.github.com>
2024-07-23 17:56:51 +0100

    Demo for git rebase

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,5 +1,6 @@
 public class Helloworld {
     public static void main( String[] args ) {
         System.out.println("This is commit from feature into master");
+        System.out.println("This demo is for git rebase from intelij");
     }
 }
