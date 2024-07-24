532b5740f686673b3b78fad5662fdbc219208a95
Nidhi Lunagariya <112748757+Nidhi16797@users.noreply.github.com>
2024-07-24 23:36:42 +0100

    Update Helloworld.java

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld {
-    public static void main( String[] args ) {
+    public staticc void main( String[] args ) {
         System.out.println("This is commit from feature into master");
         System.out.println("This demo is for git rebase from eclipse");
     }
